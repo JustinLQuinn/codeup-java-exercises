c0f39b27459192139388142ea53f3dad86870346
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-01 16:56:01 -0500

    almost finished Java I exercise. finished most

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class HelloWorld {
-}
+import javax.print.DocFlavor;
+import java.awt.geom.Arc2D;
+
+public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.print("Hello, World!");
+            System.out.print("\n");
+
+            int myFavoriteNumber = 23;
+
+            System.out.println(myFavoriteNumber);
+            System.out.print("\n");
+
+            String myString = ("Whqt can we do to help today?");
+            System.out.println(myString);
+            System.out.print("\n");
+
+            myString = ("3.14159");
+            System.out.println(myString);
+            System.out.print("\n");
+
+//            myString = (3.14159);
+//            System.out.println(myString);
+//            System.out.print("\n");
+
+//            Long myNumber;
+//            System.out.println(myNumber);
+//            System.out.print("\n");
+
+//            Long myNumber = 3.14;
+//            System.out.println(myNumber);
+//            System.out.print("\n");
+
+            long myNumber = 123L;
+            System.out.println(myNumber);
+            System.out.print("\n");
+
+//            myNumber = 123;
+//            System.out.println(myNumber);
+//            System.out.print("\n");
+
+            float myNumber = 3.14;
+            System.out.println(myNumber);
+            System.out.print("\n");
+
+
+        }
+
+    };
+
