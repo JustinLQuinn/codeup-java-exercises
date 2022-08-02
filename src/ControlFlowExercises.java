54d9b772e175eb9df9e468cc62f19c6477308de9
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-02 15:15:38 -0500

    finished 1a for exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,14 @@
+
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
+for(int i = 5; i <= 15; i += 1){
+    System.out.print(i + "\t");
+            if(i == 15){
+                break;
+            }
+
+        }
     }
 }
