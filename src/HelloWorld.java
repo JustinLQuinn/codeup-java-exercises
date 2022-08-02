2dd4d710f02a35764769ab301247b720a409d191
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-02 14:19:41 -0500

    started Control Statements Exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,23 +1,23 @@
 import javax.print.DocFlavor;
 import java.awt.geom.Arc2D;
-
+import java.util.Scanner;
 public class HelloWorld {
         public static void main(String[] args) {
             System.out.print("Hello, World!");
             System.out.print("\n");
 
-            int myFavoriteNumber = 23;
-
-            System.out.println(myFavoriteNumber);
-            System.out.print("\n");
-
-            String myString = ("Whqt can we do to help today?");
-            System.out.println(myString);
-            System.out.print("\n");
-
-            myString = ("3.14159");
-            System.out.println(myString);
-            System.out.print("\n");
+//            int myFavoriteNumber = 23;
+//
+//            System.out.println(myFavoriteNumber);
+//            System.out.print("\n");
+//
+//            String myString = ("Whqt can we do to help today?");
+//            System.out.println(myString);
+//            System.out.print("\n");
+//
+//            myString = ("3.14159");
+//            System.out.println(myString);
+//            System.out.print("\n");
 
 //            myString = (3.14159);
 //            System.out.println(myString);
@@ -31,19 +31,42 @@ public class HelloWorld {
 //            System.out.println(myNumber);
 //            System.out.print("\n");
 
-            long myNumber = 123L;
-            System.out.println(myNumber);
-            System.out.print("\n");
+//            long myNumber = 123L;
+//            System.out.println(myNumber);
+//            System.out.print("\n");
 
 //            myNumber = 123;
 //            System.out.println(myNumber);
 //            System.out.print("\n");
 
-            float myNumber = 3.14;
-            System.out.println(myNumber);
-            System.out.print("\n");
+//            float myNumber;
+//            myNumber = (float) 3.14;
+//            System.out.println(myNumber);
+//            System.out.print("\n");
 
+//            int x = 5;
+//            System.out.println(x);
 
+//            int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+
+//            int x = 5;
+//            System.out.println(++x);
+//            System.out.println(x);
+//            the (x++) increments behind the function, so you must then redeclare the variable.
+//            In (++x) it increments in front of the variable so you see the outcome apon call.
+
+//            String theNumberThree = "three";
+//            Object o = theNumberThree;
+//            int three = (int) o;
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter something: ");
+            String userInput = scanner.nextLine();
+
+            System.out.println("You entered: --> \"" + userInput + "\" <--");
         }
 
     };
---
+++ src/consoleExercise.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class consoleExercise {
+    public static void main(String[] args) {
+//        System.out.println("hello WOrld");
+
+//        double pi = 3.14159;
+//        System.out.printf("PI in shortened version = %.2f %n", pi);
+
+//        Scanner scanner = new Scanner(System.in);
+
+//        System.out.print("Enter 3 words: ");
+//        String userInput1 = scanner.next();
+//        String userInput2 = scanner.next();
+//        String userInput3 = scanner.next();
+//
+//        System.out.printf("You entered: %s \n", userInput1);
+//        System.out.printf("You entered: %s \n", userInput2);
+//        System.out.printf("You entered: %s \n", userInput3);
+
+//        System.out.print("Enter a sentence: ");
+//        String userInput = scanner.nextLine();
+//        System.out.printf("You entered: %s \n", userInput);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter width and length of room in feet: ");
+        String width = scanner.nextLine();
+        int number1 = parseInt(width);
+        String heigth = scanner.nextLine();
+        int number2 = parseInt(width);
+//        If you use this (Integer.parseInt(width)); you do not have to have an import statement.
+//        int number2 = Integer.parseInt(width);
+        int total = number1 * number2;
+        System.out.printf("The perimeter: %s \n", total);
+// this clears out the scanner
+        scanner.nextLine();
+
+    }
+}
