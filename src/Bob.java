4f378d5f9155e56d1ddc73421744309dc2a07a32
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-03 15:05:38 -0500

    got the if statements done for Bob's answers

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
-        System.in.mark("Hey");
+        String bobHello = ("Hi, let's talk...");
+            System.out.println("Bob: " + bobHello);
+        Scanner scanner = new Scanner(System.in);
+            System.out.print("Talk to Bob");
+        String uI = scanner.nextLine();
+        if(uI.endsWith("?")){
+            System.out.println("Sure.");
+        } else if (uI.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (uI.equals("")) {
+            System.out.println("Fine. Be that way!");
+        }else{
+            System.out.println("Whatever.");
+        }
+
+
     }
 }
--- src/consoleExercise.java
+++ src/consoleExercise.java
@@ -9,14 +9,14 @@ public class consoleExercise {
 //        double pi = 3.14159;
 //        System.out.printf("PI in shortened version = %.2f %n", pi);
 
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-//        System.out.print("Enter 3 words: ");
-//        String userInput1 = scanner.next();
+        System.out.print("Enter 3 words: ");
+        String userInput1 = scanner.nextLine();
 //        String userInput2 = scanner.next();
 //        String userInput3 = scanner.next();
 //
-//        System.out.printf("You entered: %s \n", userInput1);
+        System.out.printf("Bob: " + "Ok");
 //        System.out.printf("You entered: %s \n", userInput2);
 //        System.out.printf("You entered: %s \n", userInput3);
 
@@ -24,18 +24,20 @@ public class consoleExercise {
 //        String userInput = scanner.nextLine();
 //        System.out.printf("You entered: %s \n", userInput);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter width and length of room in feet: ");
-        String width = scanner.nextLine();
-        int number1 = parseInt(width);
-        String heigth = scanner.nextLine();
-        int number2 = parseInt(width);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter width and length of room in feet: ");
+//        String width = scanner.nextLine();
+//        int number1 = parseInt(width);
+//        String heigth = scanner.nextLine();
+//        int number2 = parseInt(width);
 //        If you use this (Integer.parseInt(width)); you do not have to have an import statement.
 //        int number2 = Integer.parseInt(width);
-        int total = number1 * number2;
-        System.out.printf("The perimeter: %s \n", total);
+//        int total = number1 * number2;
+//        System.out.printf("The perimeter: %s \n", total);
 // this clears out the scanner
         scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Talk to Bob");
 
     }
 }
