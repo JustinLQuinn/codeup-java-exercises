0e52be97a88bb29e4c7dc29f02c9faf8c878020d
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-03 09:50:17 -0500

    finished 1b third section, tried both methods for squaring the numbers.was able to get the same outcome with both methods.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,13 +2,16 @@
 import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 105;
+        int i = 2;
+        System.out.println(i);
         do {
-                i -= 5;
+//            both of these will work. prefer the i*i method.
+                int p = i * i;
+                i = p;
+//            double p = Math.pow(i,2);
+//            i = (int)p;
             System.out.println(i);
-                    continue;
-
-
-        } while (i >= -5);
+                continue;
+        } while (i < 300);
     }
 }
