61748afd9c98730d0e54bbf3f256cbb2f40ead40
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-03 11:22:49 -0500

    completed 2 in exercise. 2 is FizzBuzz check. calculations were correct, just had to correctly organize the checks to get desired outcome. Otherwise one check would void out another check.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,10 +2,15 @@
 import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i=2; i < 1000000; i *= i) {
-            System.out.println(i);
-            if(i >60000){
-                break;
+        for(int i=1; i <= 100; i++) {
+            if(i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
             }
         }
     }
