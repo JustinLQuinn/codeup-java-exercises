946ac31ee89b49c126748674c815981dee71b1be
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-03 17:08:50 -0500

    started Methods Exercise - first push

--- src/Bob.java
+++ src/Bob.java
@@ -3,19 +3,22 @@ public class Bob {
     public static void main(String[] args) {
         String bobHello = ("Hi, let's talk...");
             System.out.println("Bob: " + bobHello);
-        Scanner scanner = new Scanner(System.in);
-            System.out.print("Talk to Bob");
-        String uI = scanner.nextLine();
-        if(uI.endsWith("?")){
-            System.out.println("Sure.");
-        } else if (uI.endsWith("!")) {
-            System.out.println("Whoa, chill out!");
-        } else if (uI.equals("")) {
-            System.out.println("Fine. Be that way!");
-        }else{
-            System.out.println("Whatever.");
+        for (int i = 0; i < 300; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Talk to Bob: ");
+            String uI = scanner.nextLine();
+            if (uI.endsWith("?")) {
+                System.out.println("Bob: Yeah, same here.");
+            } else if (uI.endsWith("!")) {
+                System.out.println("Bob: Whoa, chill out!");
+            } else if (uI.equals("")) {
+                System.out.println("Bob: Fine..Be that way!");
+            } else if (uI.equalsIgnoreCase("bye") || uI.equalsIgnoreCase("exit") || uI.equalsIgnoreCase("close")) {
+                System.out.println("Bob: Nice talking with you. See you laterz");
+                break;
+            } else {
+                System.out.println("Bob: Whatever.");
+            }
         }
-
-
     }
 }
---
+++ src/MethodsExercises.java
@@ -0,0 +1,8 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.print(Add(12, 32));
+    }
+    public static int Add(int num1, int num2){
+return num1 + num2;
+    }
+}
