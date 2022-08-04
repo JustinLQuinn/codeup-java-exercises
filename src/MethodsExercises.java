1d025b00112dd519081c1a638c2f8eed5e3ffd89
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-04 14:15:59 -0500

    updated my recursion to referance original call for min and max values.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -34,7 +34,7 @@ public class MethodsExercises {
         if (userInt < min || userInt > max) {
             System.out.println("Not correct number, please try again.\n");
             Scanner scanner = new Scanner(System.in);
-            int userInput = getInteger(1, 10, scanner.nextInt());
+            int userInput = getInteger(min, max, scanner.nextInt());
         } else {
             System.out.println(userInt);
 
