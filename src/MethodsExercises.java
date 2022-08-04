0202aa1185f548327cef95c432dc60c66c780cbb
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-04 08:55:51 -0500

    finished numer 1 in exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,8 +1,20 @@
 public class MethodsExercises {
     public static void main(String[] args) {
-        System.out.print(Add(12, 32));
+//        System.out.print(Add(12, 32));
+//        System.out.print(Subtract(12, 32));
+//        System.out.print(Multiply(12, 32));
+//        System.out.println(Divide(40, 2));
     }
     public static int Add(int num1, int num2){
-return num1 + num2;
+        return num1 + num2;
+    }
+    public static int Subtract(int num1, int num2){
+        return num1 - num2;
+    }
+    public static int Multiply(int num1, int num2){
+        return num1 * num2;
+    }
+    public static int Divide(int num1, int num2){
+        return num1 / num2;
     }
 }
