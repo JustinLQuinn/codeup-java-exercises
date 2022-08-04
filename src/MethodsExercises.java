dfb1618ef44842d895b35ce591cbe7aed2a4f19e
Justin Quinn <Justin.Quinn09@gmail.com>
2022-08-04 14:10:16 -0500

    finished number 2 in Methods Exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,20 +1,46 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static void main(String[] args) {
 //        System.out.print(Add(12, 32));
 //        System.out.print(Subtract(12, 32));
 //        System.out.print(Multiply(12, 32));
 //        System.out.println(Divide(40, 2));
+
+        System.out.print("Enter a number between 1 and 10: ");
+        Scanner scanner = new Scanner(System.in);
+        int userInput = getInteger(1, 10, scanner.nextInt());
+
+
     }
-    public static int Add(int num1, int num2){
+
+    public static int Add(int num1, int num2) {
         return num1 + num2;
     }
-    public static int Subtract(int num1, int num2){
+
+    public static int Subtract(int num1, int num2) {
         return num1 - num2;
     }
-    public static int Multiply(int num1, int num2){
+
+    public static int Multiply(int num1, int num2) {
         return num1 * num2;
     }
-    public static int Divide(int num1, int num2){
+
+    public static int Divide(int num1, int num2) {
         return num1 / num2;
     }
+
+    public static int getInteger(int min, int max,int userInt) {
+        if (userInt < min || userInt > max) {
+            System.out.println("Not correct number, please try again.\n");
+            Scanner scanner = new Scanner(System.in);
+            int userInput = getInteger(1, 10, scanner.nextInt());
+        } else {
+            System.out.println(userInt);
+
+        }
+        return userInt;
+    }
+
 }
+
